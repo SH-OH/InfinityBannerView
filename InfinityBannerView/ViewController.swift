85c291931b03c425bb3601f06450ba37bbb99c4f
SH-OH <dhtkdgh429@gmail.com>
2020-04-08 16:32:06 +0900

    Initial Commit

---
+++ InfinityBannerView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InfinityBannerView
+//
+//  Created by Oh Sangho on 2020/04/08.
+//  Copyright © 2020 Oh Sangho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
